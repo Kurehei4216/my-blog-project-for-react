bbf151bf0f4bb341919dc33dae2ddadba12e03ae
榑林晃平 <kouhei.kurebayashi@goals.co.jp>
2024-06-02 16:13:10 +0900

    webpack導入

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
-import { fixupConfigRules } from '@eslint/compat';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
 
 export default [
   { languageOptions: { globals: globals.browser } },
