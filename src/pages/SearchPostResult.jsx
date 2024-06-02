08fdb3c3a84f4c91409c53bcc9e7b4696de1f5de
榑林晃平 <kouhei.kurebayashi@goals.co.jp>
2024-06-02 19:26:41 +0900

    [update] eslintの修正

--- src/pages/SearchPostResult.jsx
+++ src/pages/SearchPostResult.jsx
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router-dom";
 const SearchPostResult = (props) => {
   const [searchedPosts, setSearchedPosts] = useState([]);
   const history = useNavigate();
+  const [currentPage, setCurrentPage] = useState(1);
+  const perPageCount = 10;
   const { posts, keyword } = props;
 
   const toPostDetail = (postId) => {
@@ -12,11 +14,32 @@ const SearchPostResult = (props) => {
     history(`/post/${postId}`);
   };
 
+  const currentPagePosts = () => {
+    const indexOfLastPost = currentPage * perPageCount;
+    const indexOfFirstPost = indexOfLastPost - perPageCount;
+    const currentPosts = posts.flat().slice(indexOfFirstPost, indexOfLastPost);
+    return currentPosts.reduce((acc, post, i, arr) => {
+      // 添え字が割り切れる場合 → 偶数の場合
+      if (i % 2 === 0) {
+        const pair = [post];
+        if (arr[i + 1]) pair.push(arr[i + 1]);
+        acc.push(pair);
+      }
+      return acc;
+    }, []);
+  };
+
+  const currentPosts = currentPagePosts();
+
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+  };
+
   return (
     <Grid item xs={8}>
       <h3>{`「${keyword}の検索結果」`}</h3>
       <Grid container spacing={3}>
-        {posts.map((postElements) => (
+        {currentPosts.map((postElements) => (
           <>
             {postElements.map((post) => (
               // eslint-disable-next-line react/jsx-key
@@ -46,7 +69,13 @@ const SearchPostResult = (props) => {
           textAlign: "center",
         }}
       >
-        <Pagination count={10} variant="outlined" color="primary" />
+        <Pagination
+          count={Math.ceil(posts.length / perPageCount)}
+          page={currentPage}
+          onChange={handlePageChange}
+          variant="outlined"
+          color="primary"
+        />
       </Grid>
     </Grid>
   );
