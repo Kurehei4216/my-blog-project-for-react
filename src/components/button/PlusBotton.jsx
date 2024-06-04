863f43562ce706a407bcf941378c7f657c78b063
榑林晃平 <kouhei.kurebayashi@goals.co.jp>
2024-06-05 03:06:49 +0900

    [update] 管理画面の新規作成ボタンを作成

---
+++ src/components/button/PlusBotton.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { IconButton, useTheme } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+
+const AddPostButton = (props) => {
+  const { redirectPath, style } = props;
+  const theme = useTheme();
+  const history = useNavigate();
+  const handleClick = () => {
+    // ボタンがクリックされたときの処理を記述する
+    history(redirectPath);
+    console.log("ボタンがクリックされました");
+  };
+
+  return (
+    <IconButton
+      onClick={handleClick}
+      color="primary"
+      aria-label="add post"
+      style={style}
+    >
+      <AddIcon
+        style={{
+          backgroundColor: theme.palette.primary.main,
+          color: "white",
+          borderRadius: "50%",
+          width: "64px",
+          height: "64px",
+        }}
+      />
+    </IconButton>
+  );
+};
+
+export default AddPostButton;
