9c5b517743477546a9c393d49b19cb3e32155b36
榑林晃平 <kouhei.kurebayashi@goals.co.jp>
2024-06-05 03:09:03 +0900

    [update] 各画面の動線を設置

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import PostCreate from "./pages/Admin/PostCreate";
+import { CategoryPosts } from "./pages/CategoryPosts";
 import AdminPostDetail from "./pages/Admin/PostDetail";
 import AdminLayout from "./pages/Admin/Layout";
 import AdminPostList from "./pages/Admin/PostList";
 import AdminPostEdit from "./pages/Admin/PostEdit";
+import { BreadcrumbProvider } from "./context/BreadcrumbContext";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { PostDetail } from "./pages/PostDetail";
@@ -13,58 +15,68 @@ import { PostDetail } from "./pages/PostDetail";
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Layout>
-                <Home />
-              </Layout>
-            }
-          />
-          <Route
-            path="/post/:postId"
-            element={
-              <Layout>
-                <PostDetail />
-              </Layout>
-            }
-          />
-          <Route
-            path="/admin/post/create"
-            element={
-              <AdminLayout>
-                <PostCreate />
-              </AdminLayout>
-            }
-          />
-          <Route
-            path="/admin/post/:postId"
-            element={
-              <AdminLayout>
-                <AdminPostDetail />
-              </AdminLayout>
-            }
-          />
-          <Route
-            path="/admin/post/:postId/edit"
-            element={
-              <AdminLayout>
-                <AdminPostEdit />
-              </AdminLayout>
-            }
-          />
-          <Route
-            path="/admin/posts"
-            element={
-              <AdminLayout>
-                <AdminPostList />
-              </AdminLayout>
-            }
-          />
-        </Routes>
-      </Router>
+      <BreadcrumbProvider>
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Layout>
+                  <Home />
+                </Layout>
+              }
+            />
+            <Route
+              path="/post/:postId"
+              element={
+                <Layout>
+                  <PostDetail />
+                </Layout>
+              }
+            />
+            <Route
+              path="/admin/post/create"
+              element={
+                <AdminLayout>
+                  <PostCreate />
+                </AdminLayout>
+              }
+            />
+            <Route
+              path="/admin/post/:postId"
+              element={
+                <AdminLayout>
+                  <AdminPostDetail />
+                </AdminLayout>
+              }
+            />
+            <Route
+              path="/admin/post/:postId/edit"
+              element={
+                <AdminLayout>
+                  <AdminPostEdit />
+                </AdminLayout>
+              }
+            />
+            <Route
+              path="/admin/posts"
+              element={
+                <AdminLayout>
+                  <AdminPostList />
+                </AdminLayout>
+              }
+            />
+            <Route
+              path="/category/:name"
+              element={
+                <Layout>
+                  <CategoryPosts />
+                </Layout>
+              }
+            />
+          </Routes>
+        </Router>
+      </BreadcrumbProvider>
     </>
   );
 }
